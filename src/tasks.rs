795f71abd8a4ad2f1e2802f8472739f052067fa8
MutantBaby <shaheryarbakir998877@gmail.com>
2023-12-23 22:17:13 +0500

    added tasks

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,7 +1,11 @@
+use chrono::Local;
 use chrono::{serde::ts_seconds, DateTime, Utc};
 use serde::{Deserialize, Serialize};
-use std::io::Result;
+use std::fs::{File, OpenOptions};
+use std::io::{Error, ErrorKind, Result, Seek, SeekFrom};
 use std::path::PathBuf;
+
+use std::fmt;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Task {
@@ -17,14 +21,79 @@ impl Task {
     }
 }
 
+impl fmt::Display for Task {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let created_at: chrono::format::DelayedFormat<chrono::format::StrftimeItems<'_>> =
+            self.created_at.with_timezone(&Local).format("%F %H:%M");
+
+        write!(f, "{:<50} [{}]", self.text, created_at)
+    }
+}
+
 pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
+    // Open the file.
+    let mut file: File = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(journal_path)?;
+
+    let mut tasks: Vec<Task> = collect_tasks(&mut file)?;
+
+    tasks.push(task);
+
+    serde_json::to_writer(file, &tasks)?;
+
     Ok(())
 }
 
 pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> {
+    // Open the file.
+    let mut file: File = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open(journal_path)?;
+
+    let mut tasks: Vec<Task> = collect_tasks(&mut file)?;
+
+    if task_position == 0 || task_position > tasks.len() {
+        return Err(Error::new(ErrorKind::InvalidInput, "Invalid Task ID"));
+    }
+
+    tasks.remove(task_position - 1);
+    file.seek(SeekFrom::Start(0))?;
+    file.set_len(0)?;
+
+    serde_json::to_writer(file, &tasks)?;
     Ok(())
 }
 
 pub fn list_tasks(journal_path: PathBuf) -> Result<()> {
+    // Open the file.
+    let file: File = OpenOptions::new().read(true).open(journal_path)?;
+    // Parse the file and collect the tasks.
+    let tasks: Vec<Task> = collect_tasks(&file)?;
+
+    if tasks.is_empty() {
+        println!("Task list is empty");
+    } else {
+        for (i, task) in tasks.iter().enumerate() {
+            println!("{}: [{}] - {}", i + 1, task.created_at, task.text);
+        }
+    }
+
     Ok(())
+}
+
+fn collect_tasks(mut file: &File) -> Result<Vec<Task>> {
+    file.seek(SeekFrom::Start(0))?; // Rewind the file before.
+
+    let tasks: Vec<Task> = match serde_json::from_reader(file) {
+        Ok(tasks) => tasks,
+        Err(e) if e.is_eof() => Vec::new(),
+        Err(e) => Err(e)?,
+    };
+
+    file.seek(SeekFrom::Start(0))?; // Rewind the file after.
+    Ok(tasks)
 }
